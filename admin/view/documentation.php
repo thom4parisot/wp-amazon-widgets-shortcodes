216087462bc7a514a42fd31796c073294cc6207a
oncletom <thomas@oncle-tom.net>
2008-07-20 16:54:09 +0000

    * inlined documentation  * documentation translation in French
    git-svn-id: http://plugins.svn.wordpress.org/amazon-widgets-shortcodes/trunk@55763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/view/documentation.php
+++ admin/view/documentation.php
@@ -2,33 +2,108 @@
   <h3><?php _e('Amazon Widgets Shortcodes documentation', 'awshortcode') ?></h3>
 
   <div class="inside">
- * **Shortcode**: [amazon-carrousel][/amazon-carrousel]
- * **CSS Class**: `awshortcode-carrousel`
- * **Options**:
-  * *align* [left, right, center]: align the widget on the desired way
-  * *bgcolor* (CSS hexa or code value): customize the background color of the widget
-  * *height* (value in pixels): height of the animation
-  * *width* (value in pixel): width of the animation
- * **Content**:
-  * URI provided in the code to paste, usually starting by `http://ws.amazon.com/widgets/`
- * **Example**: `[amazon-carrousel]http://ws.amazon.fr/widgets/q?ServiceVersion=20070822&MarketPlace=FR&ID=V20070822%2FFR%2Fonctom-21%2F8010%2Ffc64116b-6b59-444b-b4ee-074a4adecf57&Operation=NoScript[/amazon-carrousel]`
+    <h4><?php _e('Amazon Carrousel', 'awshortcode') ?></h4>
+    <ul>
+      <li>
+        <strong><?php _e('Shortcode:', 'awshortcode') ?></strong>
+        <code>[amazon-carrousel][/amazon-carrousel]</code>
+      </li>
+      <li>
+        <strong><?php _e('Example:', 'awshortcode') ?></strong>
+        <code>[amazon-carrousel align="right"]fc64116b-6b59-444b-b4ee-074a4adecf57[/amazon-carrousel]</code>
+      </li>
+      <li>
+        <strong><?php _e('Options:', 'awshortcode') ?></strong>
+        <ul>
+          <li>
+            <code>align</code>:
+            {left, right, center}
+            <?php _e('align the widget on the desired way', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>bgcolor</code>:
+            [<?php _e('CSS hexa or code value', 'awshortcode') ?>]
+            <?php _e('customize the background color of the widget', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>height</code>:
+            [<?php _e('value in pixels', 'awshortcode') ?>]
+            <?php _e('height of the animation', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>width</code>:
+            [<?php _e('value in pixels', 'awshortcode') ?>]
+            <?php _e('width of the animation', 'awshortcode') ?>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <strong><?php _e('Content:', 'awshortcode') ?></strong>
+        <?php _e('Carrousel ID, contained in HTML parameter value like this:', 'awshortcode') ?>
+        <code>Player_<span style="background: #ffc">fc64116b-6b59-444b-b4ee-074a4adecf57</span></code>.
+      </li>
+    </ul>
 
-= Amazon Product =
-For now, it only handles this widget in the "Text + picture" version.
-
- * **Shortcode**: [amazon-product][/amazon-product]
- * **CSS Class**: `awshortcode-product`
- * **Options**:
-  * *align* [left, right, center]: align the widget on the desired way
-  * *alink* (CSS hexa or code value): hypertext link color
-  * *bgcolor* (CSS hexa or code value): customize the background color of the widget
-  * *bordercolor* (CSS hexa or code value): border color of the widget
-  * *height* (value in pixels): height of the frame
-  * *small* [0, 1]: display a small picture or a big one (0 by default)
-  * *target* (string): window name to open the link in (_blank by default)
-  * *width* (value in pixel): width of the frame
- * **Content**:
-  * ASIN code of the product 
- * **Example**: `[amazon-product]0596529309[/amazon-product]`
+    <h4><?php _e('Amazon Product', 'awshortcode') ?></h4>
+    <p><?php _e('For now, it only handles this widget in the "Text + picture" version.', 'awshortcode') ?></p>
+    <ul>
+      <li>
+        <strong><?php _e('Shortcode:', 'awshortcode') ?></strong>
+        <code>[amazon-product][/amazon-product]</code>
+      </li>
+      <li>
+        <strong><?php _e('Example:', 'awshortcode') ?></strong>
+        <code>[amazon-product align="right"]0596529309[/amazon-product]</code>
+      </li>
+      <li>
+        <strong><?php _e('Options:', 'awshortcode') ?></strong>
+        <ul>
+          <li>
+            <code>align</code>:
+            {left, right, center}
+            <?php _e('align the widget on the desired way', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>alink</code>:
+            [<?php _e('CSS hexa or code value', 'awshortcode') ?>]
+            <?php _e('align the widget on the desired way', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>bgcolor</code>:
+            [<?php _e('CSS hexa or code value', 'awshortcode') ?>]
+            <?php _e('customize the background color of the widget', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>bordercolor</code>:
+            [<?php _e('CSS hexa or code value', 'awshortcode') ?>]
+            <?php _e('border color of the widget', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>height</code>:
+            [<?php _e('value in pixels', 'awshortcode') ?>]
+            <?php _e('height of the frame', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>small</code>:
+            {0, 1}
+            <?php _e('display a small picture or a big one (0 by default)', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>target</code>:
+            [<?php _e('string', 'awshortcode') ?>]
+            <?php _e('window name to open the link in (_blank by default)', 'awshortcode') ?>
+          </li>
+          <li>
+            <code>width</code>:
+            [<?php _e('value in pixels', 'awshortcode') ?>]
+            <?php _e('width of the frame', 'awshortcode') ?>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <strong><?php _e('Content:', 'awshortcode') ?></strong>
+        <?php _e('ASIN code of the product.', 'awshortcode') ?>
+      </li>
+    </ul>
   </div>
 </div>
